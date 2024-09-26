243410eb87709e2decb2717b459ad7d9eba40a77
fukuda0801 <nagisano0801@gmail.com>
2024-09-26 10:00:08 +0900

    feat: ユーザー登録ページにてReact Hook Form 使用。

--- src/pages/user/register.tsx
+++ src/pages/user/register.tsx
@@ -3,25 +3,86 @@ import InputText from "@/components/user/InputText";
 import Title from "@/components/user/Title";
 import styles from "../../styles/pages/user/register.module.css";
 import Button from "@/components/utils/Button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "@/lib/validation";
+
+type RegisterProps = {
+  name: string;
+  email: string;
+  gender: string;
+  age: string;
+  password: string;
+  confirmedPassword: string;
+};
 
 const Register = () => {
+  // React Hook Formにてフォーム管理、zodにてバリデーション実装
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterProps>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  // 新規登録処理
+  const onSubmit = (data: RegisterProps) => {
+    console.log("フォームが送信されました", data);
+  };
+
   return (
     <main className={styles.registerContent}>
-      <form className={styles.registerForm}>
+      <form className={styles.registerForm} onSubmit={handleSubmit(onSubmit)}>
         <Title title="アカウント登録" />
-        <InputText name="name" label="ユーザー名" type="text" />
-        <InputText name="email" label="メールアドレス" type="text" />
+        <InputText
+          name="name"
+          label="ユーザー名"
+          type="text"
+          register={register("name")}
+          errorMessage={errors.name?.message}
+        />
+        <InputText
+          name="email"
+          label="メールアドレス"
+          type="text"
+          register={register("email")}
+          errorMessage={errors.email?.message}
+        />
         <div className={styles.inputRadioGroup}>
-          <label>性別</label>
-          <InputRadio name="gender" label="男性" value="male" />
-          <InputRadio name="gender" label="女性" value="female" />
-          <InputRadio name="gender" label="その他" value="other" />
+          <div className={styles.inputRadioContent}>
+            <label>性別</label>
+            <InputRadio label="男性" value="男性" register={register("gender")} checked={true} />
+            <InputRadio label="女性" value="女性" register={register("gender")} />
+            <InputRadio label="その他" value="その他" register={register("gender")} />
+          </div>
+          {errors.gender && <p className={styles.errorMessage}>{errors.gender.message}</p>}
         </div>
-        <InputText name="age" label="年齢" type="number" />
-        <InputText name="password" label="パスワード" type="password" />
-        <InputText name="confirmedPassword" label="確認用パスワード" type="password" />
+        <InputText
+          name="age"
+          label="年齢"
+          type="number"
+          register={register("age", {
+            setValueAs: (value) => (value === "" ? undefined : parseInt(value, 10)),
+          })}
+          errorMessage={errors.age?.message}
+        />
+        <InputText
+          name="password"
+          label="パスワード"
+          type="password"
+          register={register("password")}
+          errorMessage={errors.password?.message}
+        />
+        <InputText
+          name="confirmedPassword"
+          label="確認用パスワード"
+          type="password"
+          register={register("confirmedPassword")}
+          errorMessage={errors.confirmedPassword?.message}
+        />
         <div className={styles.buttonGroup}>
-          <Button type="button" label="登録" variant="primary" />
+          <Button type="submit" label="登録" variant="primary" disabled={isSubmitting} />
           <Button type="button" label="戻る" variant="secondary" />
         </div>
       </form>
