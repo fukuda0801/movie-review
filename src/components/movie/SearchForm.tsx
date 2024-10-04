e6b8f82570da98fdc6e732a2685f84492abb221e
fukuda0801 <nagisano0801@gmail.com>
2024-10-04 15:01:25 +0900

    fix: あいまい検索コンポーネントのタイトルをpropsで管理するように修正

--- src/components/movie/SearchForm.tsx
+++ src/components/movie/SearchForm.tsx
@@ -1,9 +1,11 @@
 import styles from "@/styles/pages/movie/[id]/index.module.css";
 import Button from "../utils/Button";
+import { SearchFormProps } from "@/types/movie";
 
-const SearchForm = () => {
+const SearchForm = ({ title }: SearchFormProps) => {
   return (
     <div className={styles.searchForm}>
+      <h2>{title}</h2>
       <form className={styles.searchFormContent}>
         <input type="text" placeholder="映画検索" />
         <Button type="submit" label="検索" variant="third" />
