d5cc9ff975b9f4b9612230cd541a0db7dc317b7a
fukuda0801 <nagisano0801@gmail.com>
2024-10-06 22:07:10 +0900

    feat: 公開映画一覧ページ作成

---
+++ src/pages/movie/playing/index.tsx
@@ -0,0 +1,46 @@
+import ResultMovieGroup from "@/components/movie/ResultMovieGroup";
+import ResultPagination from "@/components/movie/ResultPagination";
+import ResultTitle from "@/components/movie/ResultTitle";
+import styles from "@/styles/pages/movie/playing/index.module.css";
+import { GetServerSideProps } from "next";
+
+type PlayingMovie = {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+};
+
+type PlayingMovieProps = {
+  movieDetails: PlayingMovie[];
+  currentPage: number;
+  totalPage: number;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const page = parseInt(context.query.page as string, 10) || 1;
+
+  const res = await fetch(`http://localhost:3000/api/movie/resultPlayingMovie?page=${page}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      movieDetails: data.movieDetails,
+      currentPage: data.currentPage,
+      totalPage: data.totalPage,
+    },
+  };
+};
+
+const Index = ({ movieDetails, currentPage, totalPage }: PlayingMovieProps) => {
+  return (
+    <main>
+      <ResultTitle title="上映中映画一覧" currentPage={currentPage} totalPage={totalPage} />
+      <ResultMovieGroup movieDetails={movieDetails} />
+      <ResultPagination currentPage={currentPage} totalPage={totalPage} url="playing" />
+    </main>
+  );
+};
+
+export default Index;
