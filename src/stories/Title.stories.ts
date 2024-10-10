4f1c5a65a6b415b6434e56e1af607eed5ed94dba
fukuda0801 <nagisano0801@gmail.com>
2024-10-10 11:53:14 +0900

    feat: ResultTitleコンポーネントをstorybookに追加

---
+++ src/stories/Title.stories.ts
@@ -0,0 +1,14 @@
+import ResultTitle from "@/components/movie/ResultTitle";
+
+export default {
+  title: "ResultTitle",
+  component: ResultTitle,
+};
+
+export const firstPage = {
+  args: {
+    title: "タイトル",
+    currentPage: 2,
+    totalPage: 3,
+  },
+};
